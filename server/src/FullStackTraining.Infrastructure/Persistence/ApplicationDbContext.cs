8190ea766cc7ae490f8c9b249047eaacfb50cf72
Pete Deffendol <pete.deffendol@centeva.com>
2024-10-02 16:34:47 -0600

    Set up EF Core and repository implementation

---
+++ server/src/FullStackTraining.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using FullStackTraining.Core.ProjectAggregate;
+using Microsoft.EntityFrameworkCore;
+
+namespace FullStackTraining.Infrastructure.Persistence;
+public class ApplicationDbContext : DbContext
+{
+    public DbSet<Project> Projects => Set<Project>();
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+        
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
---
+++ server/src/FullStackTraining.Infrastructure/Persistence/EfRepository.cs
@@ -0,0 +1,9 @@
+﻿using Centeva.DomainModeling;
+using Centeva.DomainModeling.EFCore;
+
+namespace FullStackTraining.Infrastructure.Persistence;
+public class EfRepository<T> : BaseRepository<T> where T : class, IAggregateRoot
+{
+    public EfRepository(ApplicationDbContext dbContext)
+        : base(dbContext) { }
+}
