ceed35d61b8a3afc16a48d6a248d69b7f7b0e51c
Pete Deffendol <pete.deffendol@centeva.com>
2024-10-02 16:34:15 -0600

    Add Project aggregate
    This is basic for now, needs to be expanded to be more interesting.

---
+++ server/src/FullStackTraining.Core/ProjectAggregate/Project.cs
@@ -0,0 +1,25 @@
+﻿using Ardalis.GuardClauses;
+using Centeva.DomainModeling;
+
+namespace FullStackTraining.Core.ProjectAggregate;
+public class Project : BaseEntity<Guid>, IAggregateRoot
+{
+    public string Name { get; private set; }
+    public string? Description { get; private set; }
+
+    public Project(Guid id, string name)
+    {
+        Id = Guard.Against.Default(id);
+        Name = Guard.Against.NullOrWhiteSpace(name, nameof(name));
+    }
+
+    public void Rename(string newName)
+    {
+        Name = Guard.Against.NullOrWhiteSpace(newName, nameof(newName));
+    }
+
+    public void UpdateDescription(string? description)
+    {
+        Description = description;
+    }
+}
